9b018d4fc962904ac0479eaa45406c15e1f80136
Ale Raf <raffinalexis@gmail.com>
2023-03-20 18:35:18 +0100

    backend init

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const bookRoutes = require('./routes/book');
+
+mongoose.connect('mongodb+srv://raffinalexis:8TeJjl4NSVdpfecw@cluster0.c0meqjm.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+    
+    const app = express();
+    
+    app.use(express.json());
+    
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/books', bookRoutes);
+
+module.exports = app;
---
+++ backend/routes/book.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const bookCtrl = require('../controllers/book')
+
+router.post('/', bookCtrl.createBook);
+router.put('/:id', bookCtrl.editBook);
+router.delete('/:id', bookCtrl.deleteBook);
+router.get('/:id', bookCtrl.getOneBook);
+router.get('/', bookCtrl.getAllBooks);
+
+module.exports = router;
